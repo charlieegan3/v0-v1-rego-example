8cda322a05b7137c6d1a2fcef70941d898d6b10d
Charlie Egan <charlieegan3@users.noreply.github.com>
2024-11-20 15:51:40 +0000

    Show bundle based example too

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ Rego Version: v1
 [{"expressions":[{"value":["foo"],"text":"data.example.messages","location":{"row":1,"col":1}}]}]
 Rego Version: v0
 [{"expressions":[{"value":["foo"],"text":"data.example.messages","location":{"row":1,"col":1}}]}]
+-----------------
+[{"expressions":[{"value":["bar","foo"],"text":"data.v1.messages | data.v0.messages","location":{"row":1,"col":1}}]}]
 ```
--- main.go
+++ main.go
@@ -6,10 +6,13 @@ import (
 	"fmt"
 
 	"github.com/open-policy-agent/opa/ast"
+	"github.com/open-policy-agent/opa/bundle"
 	"github.com/open-policy-agent/opa/rego"
 )
 
 func main() {
+	// first, the simple case where we just need to evaludate some rego in
+	// different versions init'ing Rego each time
 	examples := []struct {
 		RegoVersion ast.RegoVersion
 		Module      string
@@ -52,4 +55,84 @@ messages[msg] {
 
 		fmt.Println(string(bs))
 	}
+
+	fmt.Println("-----------------")
+
+	// second, the more involved case where rego can be initialized once for
+	// bundles of different versions.
+
+	mod0, err := ast.ParseModuleWithOpts(
+		"policy.rego",
+		`package v0
+
+messages[msg] {
+	msg := "foo"
+}
+`,
+		ast.ParserOptions{
+			RegoVersion: ast.RegoV0,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	mod1, err := ast.ParseModuleWithOpts(
+		"policy.rego",
+		`package v1
+
+messages contains "bar"
+`,
+		ast.ParserOptions{
+			RegoVersion: ast.RegoV1,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	bv0 := bundle.Bundle{
+		Manifest: bundle.Manifest{
+			Roots: &[]string{"examplev0"},
+		},
+		Modules: []bundle.ModuleFile{
+			{
+				Parsed: mod0,
+			},
+		},
+	}
+
+	bv0.SetRegoVersion(ast.RegoV0)
+
+	bv1 := bundle.Bundle{
+		Manifest: bundle.Manifest{
+			Roots: &[]string{"examplev1"},
+		},
+		Modules: []bundle.ModuleFile{
+			{
+				Parsed: mod1,
+			},
+		},
+	}
+
+	bv1.SetRegoVersion(ast.RegoV1)
+
+	r := rego.New(
+		rego.ParsedBundle("v0", &bv0),
+		rego.ParsedBundle("v1", &bv1),
+		// query for both v0 and v1 policies at the same time
+		rego.Query("data.v1.messages | data.v0.messages"),
+	)
+
+	rs, err := r.Eval(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	bs, err := json.Marshal(rs)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(bs))
 }
